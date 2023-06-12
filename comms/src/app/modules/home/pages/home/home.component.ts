ac4f32281ddcd3d38fa5f4521748142e9cf1b453
Aytar Akdemir <aytarakdemir99@gmail.com>
2023-06-12 21:26:31 +0300

    add notes

--- comms/src/app/modules/home/pages/home/home.component.ts
+++ comms/src/app/modules/home/pages/home/home.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import * as Tone from 'tone';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent {
+  constructor() {}
 
+  playNote(note: string) {
+    const synth = new Tone.Synth().toDestination();
+    synth.triggerAttackRelease(note, '8n');
+  }
+
+  playChord(notes: string[]) {
+    notes.forEach((note: string) => {
+      this.playNote(note);
+    });
+  }
 }
