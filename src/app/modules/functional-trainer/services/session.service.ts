4bd6c8f2f386346cc6804e9ff95e317f1b932986
Aytar Akdemir <aytarakdemir99@gmail.com>
2023-06-25 20:29:22 +0300

    implement end screen

--- src/app/modules/functional-trainer/services/session.service.ts
+++ src/app/modules/functional-trainer/services/session.service.ts
@@ -1,36 +1,57 @@
-import { Injectable, WritableSignal, Signal, signal,computed } from '@angular/core';
+import {
+  Injectable,
+  WritableSignal,
+  Signal,
+  signal,
+  computed,
+} from '@angular/core';
 import { KeyService } from 'src/app/shared/services/key/key.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class SessionService {
+  public questions: {
+    correctNoteIndex?: number;
+    wrongNoteIndexList?: number[];
+    isCorrect?: boolean;
+  }[] = [];
 
-  
-  public questions: {correctNoteIndex?: number, wrongNoteIndexList?: number[], isCorrect?: boolean}[] = [];
-  
   public state: WritableSignal<SessionState> = signal(SessionState.FirstInit);
 
   public questionCount = 20;
-  
+
   public activeQuestion = 0;
 
   SessionState = SessionState;
 
-  private selectedKey: Signal<string[]> = computed(()=>this.keySrv.selectedNoteList().map(note => {return note.slice(0, -1)} ))
+  private selectedKey: Signal<string[]> = computed(() =>
+    this.keySrv.selectedNoteList().map((note) => {
+      return note.slice(0, -1);
+    })
+  );
 
-  public activeQuestionState: {correctNoteIndex?: number, wrongNoteIndexList?: number[], isCorrect?: boolean} = {};
+  public activeQuestionState: {
+    correctNoteIndex?: number;
+    wrongNoteIndexList?: number[];
+    isCorrect?: boolean;
+  } = {};
 
   public canProceedToTheNextQuestion: boolean = false;
 
-  constructor(private keySrv: KeyService) { }
+  public results: { correctCount?: number; questionCount?: number } = {};
+
+  constructor(private keySrv: KeyService) {}
 
   public startSession(questionCount: number = 20) {
-    if(this.state() === SessionState.Active) throw new Error("Session already active");
+    if (this.state() === SessionState.Active)
+      throw new Error('Session already active');
     this.state.set(SessionState.Active);
     this.questionCount = questionCount;
+  }
 
-
+  public resetSession() {
+    this.state.set(SessionState.FirstInit);
   }
 
   public goToNextQuestion() {
@@ -41,30 +62,51 @@ export class SessionService {
     }
     this.questions.push(this.activeQuestionState);
 
-    if (this.questionCount - 1> this.activeQuestion) {
+    if (this.questionCount - 1 > this.activeQuestion) {
+      console.log(
+        'Active Question: ',
+        this.activeQuestion,
+        '\nQuestions Array: ',
+        this.questions
+      );
       this.activeQuestionState = {};
       this.activeQuestion = this.activeQuestion + 1;
       this.canProceedToTheNextQuestion = false;
-      console.log("Active Question: ", this.activeQuestion, "\nQuestions Array: ", this.questions);
     } else {
+      console.log(
+        'Active Question: ',
+        this.activeQuestion,
+        '\nQuestions Array: ',
+        this.questions
+      );
       this.endSession();
-      console.log("Active Question: ", this.activeQuestion, "\nQuestions Array: ", this.questions);
     }
   }
 
-
   public endSession() {
-    if(this.state() !== SessionState.Active) throw new Error("No active session to end");
+    if (this.state() !== SessionState.Active)
+      throw new Error('No active session to end');
     this.state.set(SessionState.AfterSession);
 
-    // Get the state ready for the next session
+    let correctCount = 0;
+    this.questions.forEach((question) => {
+      if (question.isCorrect) correctCount++;
+    });
+
+    // Assign endgame statistics to the results object here...
+    this.results = {
+      correctCount: correctCount,
+      questionCount: this.questionCount,
+    };
+
+    // ...before the session info gets deleted here in order to get ready for a new game
     this.activeQuestionState = {};
     this.canProceedToTheNextQuestion = false;
     this.activeQuestion = 0;
+    this.questions = [];
   }
 
-  public guess(correctNote: string, guessNote:string) {
-
+  public guess(correctNote: string, guessNote: string) {
     const guessIndex = this.selectedKey().indexOf(guessNote);
     const correctIndex = this.selectedKey().indexOf(correctNote);
 
@@ -76,24 +118,18 @@ export class SessionService {
 
     if (this.canProceedToTheNextQuestion) return;
 
-
     if (this.activeQuestionState.wrongNoteIndexList) {
-      if(!this.activeQuestionState.wrongNoteIndexList.includes(guessIndex)) {
-        this.activeQuestionState.wrongNoteIndexList.push(guessIndex)
+      if (!this.activeQuestionState.wrongNoteIndexList.includes(guessIndex)) {
+        this.activeQuestionState.wrongNoteIndexList.push(guessIndex);
       }
     } else {
       this.activeQuestionState.wrongNoteIndexList = [guessIndex];
     }
   }
-
-
-
-
 }
 
-
 export enum SessionState {
-  FirstInit = "firstInit",
-  Active = "active",
-  AfterSession = "afterSession"
+  FirstInit = 'firstInit',
+  Active = 'active',
+  AfterSession = 'afterSession',
 }
