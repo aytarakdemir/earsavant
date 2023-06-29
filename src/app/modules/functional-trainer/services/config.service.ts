305df3cf699acf436f7d2d55feb067b300c3e4a1
Aytar Akdemir <aytarakdemir99@gmail.com>
2023-06-30 01:02:27 +0300

    convert config boolean arrays to index arrays

--- src/app/modules/functional-trainer/services/config.service.ts
+++ src/app/modules/functional-trainer/services/config.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, WritableSignal, effect, signal } from '@angular/core';
+import { Injectable, WritableSignal, effect, signal, untracked } from '@angular/core';
 import * as _ from 'lodash';
 import { KeyService } from 'src/app/shared/services/key/key.service';
 import { WalkMode } from '../pages/functional-trainer/functional-trainer.component';
@@ -22,7 +22,45 @@ export class ConfigService {
 
   constructor(private keySrv: KeyService) {
     effect(()=> {
-      console.log(this.configObj());
+      this.configObj();
+      untracked(()=> {
+
+        let possibleNotes = this.configObj().possibleNotesConfig.reduce((acc:number[], curr:boolean, index:number) => {
+          if (curr) {
+            acc.push(index);
+          }
+          return acc;
+        }, []);
+
+        console.log(possibleNotes);
+        
+        let i = 0
+        let scaleNotes = this.configObj().scaleConfig.reduce((acc:number[], curr:boolean) => {
+          if (curr !== null) {
+            i++;
+          }
+          if (curr) {
+            acc.push(i);
+          }
+          return acc;
+        }, []);
+        
+        console.log(scaleNotes);
+
+
+
+        let chordNotes = this.configObj().chordsProgressionConfig.map((chord: {chordNotes: boolean[]}) => {
+          return chord.chordNotes.reduce((acc:number[], curr:boolean, index:number) => {
+            if (curr) {
+              acc.push(index);
+            }
+            return acc;
+          }, []);
+  
+        })
+        console.log(chordNotes);
+
+      });
     })
   }
 
